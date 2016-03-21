a747b4c378038bdbf7e86ee5f7d370c529602557
ClaytonMu <claytor0729@hotmail.com>
2016-03-21 18:13:38 +0800

    init

---
+++ MVCHomework1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomework1.Startup))]
+namespace MVCHomework1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
